66a99d1235b31ccfb63437ce8ed7540fe68cf8da
Maciej Zgadzaj <maciej.zgadzaj@gmail.com>
2015-07-08 02:55:40 -0400

    Capture/refund/cancel, cardonfile support, Request a New Order operation, refactoring

--- modules/tpp/commerce_hipay_tpp.api.php
+++ modules/tpp/commerce_hipay_tpp.api.php
@@ -8,7 +8,7 @@
  */
 
 /**
- * Allows other modules to alter hosted payment page initialization request
+ * Allows other modules to alter Initialize a Hosted Payment Page API request
  * parameters before sending them to Hipay TPP API.
  *
  * @param array $request_data
@@ -17,8 +17,76 @@
  *   An order object being paid for.
  * @param array $payment_method
  *   The payment method instance used for the payment transaction.
+ *
+ * @see http://hipay-tpp-gateway-api.readthedocs.org/en/latest/Chap3-RESTAPIResources-initializeHostedPaymentPage.html
  */
-function hook_commerce_hipay_tpp_initialize_alter(&$request_data, $order, $payment_method) {
+function hook_commerce_hipay_tpp_api_initialize_alter(&$request_data, $order, $payment_method) {
+  // No example.
+}
+
+/**
+ * Allows other modules to alter Request a New Order API request parameters
+ * before sending them to Hipay TPP API.
+ *
+ * @param array $request_data
+ *   An array of parameters being sent to Hipay TPP API.
+ * @param object $order
+ *   An order object being paid for.
+ * @param array $payment_method
+ *   The payment method instance used for the payment transaction.
+ *
+ * @see http://hipay-tpp-gateway-api.readthedocs.org/en/latest/Chap3-RESTAPIResources-requestNewOrder.html
+ */
+function hook_commerce_hipay_tpp_api_order_request_alter(&$request_data, $order, $payment_method) {
+  // No example.
+}
+
+/**
+ * Allow other modules to validate redirect feedback and change validation
+ * result.
+ *
+ * @param array $feedback
+ *   An associative array containing the Hipay API call feedback.
+ * @param object $transaction
+ *   Payment transaction to refresh.
+ * @param object $order
+ *   An order object being paid for.
+ * @param array $payment_method
+ *   The payment method instance used for the payment transaction.
+ */
+function hook_commerce_hipay_tpp_redirect_form_validate($feedback, $transaction, $order, $payment_method) {
+  // No example.
+}
+
+/**
+ * Allow other modules to validate redirect feedback and change validation
+ * result.
+ *
+ * @param array $feedback
+ *   An associative array containing the Hipay API call feedback.
+ * @param object $order
+ *   An order object being paid for.
+ * @param array $payment_method
+ *   The payment method instance used for the payment transaction.
+ */
+function hook_commerce_hipay_tpp_redirect_form_submit($feedback, $order, $payment_method) {
+  // No example.
+}
+
+/**
+ * Allows other modules to alter transaction refresh request parameters
+ * before sending them to Hipay TPP API.
+ *
+ * @param array $request_data
+ *   An array of parameters being sent to Hipay TPP API.
+ * @param object $order
+ *   An order object being paid for.
+ * @param object $transaction
+ *   Payment transaction to refresh.
+ * @param array $payment_method
+ *   The payment method instance used for the payment transaction.
+ */
+function hook_commerce_hipay_tpp_api_refresh_alter(&$request_data, $order, $transaction, $payment_method) {
   // No example.
 }
 
@@ -33,21 +101,52 @@ function hook_commerce_hipay_tpp_initialize_alter(&$request_data, $order, $payme
  * @param array $payment_method
  *   The payment method instance used for the payment transaction.
  */
-function hook_commerce_hipay_tpp_capture_alter(&$request_data, $order, $payment_method) {
+function hook_commerce_hipay_tpp_api_capture_alter(&$request_data, $order, $payment_method) {
   // No example.
 }
 
 /**
- * Allows other modules to alter transaction refresh request parameters
- * before sending them to Hipay TPP API.
+ * Allows other modules to alter refund request parameters before sending them
+ * to Hipay TPP API.
  *
  * @param array $request_data
  *   An array of parameters being sent to Hipay TPP API.
  * @param object $order
  *   An order object being paid for.
- * @param object $transaction
- *   Payment transaction to refresh.
+ * @param array $payment_method
+ *   The payment method instance used for the payment transaction.
  */
-function hook_commerce_hipay_tpp_refresh_alter(&$request_data, $order, $transaction, $payment_method) {
+function hook_commerce_hipay_tpp_api_refund_alter(&$request_data, $order, $payment_method) {
+  // No example.
+}
+
+/**
+ * Allows other modules to alter cancel request parameters before sending them
+ * to Hipay TPP API.
+ *
+ * @param array $request_data
+ *   An array of parameters being sent to Hipay TPP API.
+ * @param object $order
+ *   An order object being paid for.
+ * @param array $payment_method
+ *   The payment method instance used for the payment transaction.
+ */
+function hook_commerce_hipay_tpp_api_cancel_alter(&$request_data, $order, $payment_method) {
+  // No example.
+}
+
+/**
+ * Allow other modules to alter the payment transaction before it is saved.
+ *
+ * @param object $transaction
+ *   Payment transaction updated from received feedback before it is saved.
+ * @param array $feedback
+ *   An associative array containing the Hipay API call feedback.
+ * @param object $order
+ *   An order object being paid for.
+ * @param array $payment_method
+ *   The payment method instance used for the payment transaction.
+ */
+function hook_commerce_hipay_tpp_process_feedback_alter(&$transaction, $feedback, $order, $payment_method) {
   // No example.
 }
