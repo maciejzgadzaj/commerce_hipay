d87a5b1b5f5f8118b1b48857d7361fd3def83f04
Maciej Zgadzaj <maciej.zgadzaj@gmail.com>
2015-08-04 05:41:39 -0400

    First take on Hipay Wallet (Cashout) - added commerce_hipay_ws module

---
+++ modules/ws/commerce_hipay_ws.api.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @file
+ * This file contains no working PHP code; it exists to provide additional
+ * documentation for doxygen as well as to document hooks in the standard
+ * Drupal manner.
+ */
+
+/**
+ * Allows other modules to alter Hipay Wallet SOAP API request parameters
+ * before sending them to the API.
+ *
+ * @param array $parameters
+ *   An array of parameters being sent to Hipay Wallet API.
+ * @param string $resource
+ *   An API resource which the request is being sent to.
+ * @param array $payment_method_instance
+ *   The payment method instance used for the operation.
+ */
+function hook_commerce_hipay_ws_api_request_alter(&$parameters, $resource, $payment_method_instance) {
+  // No example.
+}
+
+/**
+ * Allows other modules to do custom processing of Hipay Wallet API response.
+ *
+ * @param array $response
+ *   An array of response parameters returned by Hipay Wallet API.
+ * @param array $parameters
+ *   An array of parameters being sent to Hipay Wallet API.
+ * @param string $resource
+ *   An API resource which the request is being sent to.
+ * @param array $payment_method_instance
+ *   The payment method instance used for the operation.
+ */
+function hook_commerce_hipay_ws_api_response($response, $parameters, $resource, $payment_method_instance) {
+  // No example.
+}
